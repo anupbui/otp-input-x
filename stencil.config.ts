631820cdc0435d3a02741c2fbb0b998476214cbd
Anup Bhatkhande <anupb239@gmail.com>
2020-08-30 09:14:56 +0530

    add react and angular output config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,19 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget, ValueAccessorConfig } from '@stencil/angular-output-target'
+const angularValueAccessorBindings: ValueAccessorConfig[] = [
+  {
+    elementSelectors: ['otp-input-x'],
+    event: 'otpChange',
+    targetAttr: 'otp',
+    type: 'text',
+  },
+  {
+    elementSelectors: ['otp-input-x'],
+    event: 'otpComplete',
+    targetAttr: 'otp',
+    type: 'text',
+  }];
+
 
 export const config: Config = {
   namespace: 'otp-input-x',
@@ -8,6 +23,18 @@ export const config: Config = {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
+    /*angularOutputTarget({
+      componentCorePackage: 'otp-input-x',
+      directivesProxyFile: './src/component/otp-input-x.tsx',
+      valueAccessorConfigs: angularValueAccessorBindings,
+    }),
+    reactOutputTarget({
+      componentCorePackage: 'otp-input-x',
+      proxiesFile: './src/component/otp-input-x.ts',
+      loaderDir: '../loader-react',
+      includePolyfills: true,
+      includeDefineCustomElements: true,
+    }),*/
     {
       type: 'dist-custom-elements-bundle'
     },
